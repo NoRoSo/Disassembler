fab197ae5151afe6f0bd61e33ac0075bf5627d05
NoRoSo <fairfieldns@gmail.com>
2023-09-27 00:51:38 -0500

    added files

---
+++ Main/team24_project1.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+)
+
+var InputFileName *string
+var OutputFileName *string
+
+func main() {
+
+	//retrieving flags -i and -o (input and output file name)
+	InputFileName = flag.String("i", "", "Gets the input file name")
+	OutputFileName = flag.String("o", "", "Gets the output file name")
+
+	flag.Parse()
+
+	readFile, err := os.Open(*InputFileName)
+
+	if err != nil { //in case it doesn't open
+		fmt.Println(err)
+		return
+	}
+
+	//reading the input of the text file line by line and inserting it into a list.
+	fileScanner := bufio.NewScanner(readFile)
+	fileScanner.Split(bufio.ScanLines)
+	var fileLines []string
+
+	for fileScanner.Scan() {
+		fileLines = append(fileLines, fileScanner.Text())
+	}
+
+	fmt.Println(fileLines)
+
+	readFile.Close()
+}
+
+func WriteOutput() {
+	file, errs := os.Create(*OutputFileName)
+	if errs != nil {
+		fmt.Println("Failed to create file:", errs)
+		return
+	}
+	defer file.Close()
+
+	_, errs = file.WriteString("Hello, world")
+	if errs != nil {
+		fmt.Println("Failed to write to file:", errs)
+		return
+	}
+	fmt.Println("Wrote to file: ", *OutputFileName)
+}
