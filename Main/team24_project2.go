6f7a7f735f55bef4e6175af8aa91dd0f3f76476c
NoRoSo <fairfieldns@gmail.com>
2023-11-25 23:19:13 -0600

    Made more additions to everything (basically skeleton code for proj 2)

--- Main/team24_project2.go
+++ Main/team24_project2.go
@@ -13,9 +13,22 @@ var InputFileName *string
 var OutputFileName *string
 var ProgramCounter = 96
 var hasBreak = false
+var cycleNum = 1
+
+var instructionMap = map[int64]Instruction{}
+var registers = []int64{}
+var maxRegisters = 32
+
+type Instruction struct {
+	instructionName string //The instruction name
+	storeRegister   int64  //The register where the output gets stored
+	targetRegister1 int64  //The first target register (if you read it, it will be the middle register)
+	targetRegister3 int64  //The first target register (if you read it, it will be the middle register)
+	immediateValue  int64  //If needed on certain instructions, use this to store immediate values (e.g: B, ADDI, SUBI, etc.)
+	programCounter  int64  //The PC of this instruction.
+}
 
 func main() {
-
 	//retrieving flags -i and -o (input and output file name)
 	InputFileName = flag.String("i", "", "Gets the input file name")
 	OutputFileName = flag.String("o", "", "Gets the output file name")
@@ -48,6 +61,10 @@ func main() {
 
 func WriteOutput(fileLine []string) {
 	file, errs := os.Create(*OutputFileName + "_dis.txt")
+	file2, errs := os.Create(*OutputFileName + "_sim.txt")
+
+	fmt.Println(outputRegisters())
+
 	if errs != nil {
 		fmt.Println("Failed to create file:", errs)
 		return
@@ -68,6 +85,32 @@ func WriteOutput(fileLine []string) {
 		}
 		ProgramCounter += 4
 	}
+
+	currentInstruction := instructionMap[int64(96)]
+	ProgramCounter = 96
+	for currentInstruction.instructionName != "BREAK" {
+		tempString2 := createSimString(currentInstruction)
+		_, errs = file2.WriteString(tempString2)
+		cycleNum++
+	}
+
+	err := file.Close()
+	if err != nil {
+		return
+	}
+
+	err = file2.Close()
+	if err != nil {
+		return
+	}
+
+}
+
+func createSimString(instruction Instruction) string {
+	simOutput := "=====================\n"
+	simOutput += fmt.Sprintf("cycle:%d\t%d\t", cycleNum, instruction.programCounter)
+
+	return simOutput
 }
 
 func CreateString(instructionCode string) string {
@@ -86,6 +129,7 @@ func CreateString(instructionCode string) string {
 		decimalNum := ConvertBinaryToDecimal(number)
 
 		formattedString := fmt.Sprintf("%s\t", opcode+" "+number)
+		instructionMap[int64(ProgramCounter)] = Instruction{programCounter: int64(ProgramCounter), instructionName: "B", immediateValue: decimalNum}
 		//38 characters
 		return formattedString + fmt.Sprintf("%s\t", strconv.FormatInt(int64(ProgramCounter), 10)) + "B\t#" + strconv.FormatInt(decimalNum, 10) + "\n"
 	}
@@ -97,7 +141,6 @@ func CreateString(instructionCode string) string {
 	if strings.Index(opcode, "10001011000") == 0 { //ADD instruction NATHAN
 
 		return R_Instruction(instructionCode, "ADD")
-
 	}
 
 	if strings.Index(opcode, "1001000100") == 0 { //ADDI instruction NATHAN
@@ -109,35 +152,11 @@ func CreateString(instructionCode string) string {
 	}
 
 	if strings.Index(opcode, "10110100") == 0 { //CBZ instruction
-		opcode = instructionCode[:8]
-		immediate := instructionCode[8:27]
-		rd := instructionCode[27:]
-
-		immNum, _ := strconv.ParseInt(rd, 2, 32)
-
-		formattedString := fmt.Sprintf("%s\t", opcode+" "+immediate+" "+rd)
-
-		decimalNum := ConvertBinaryToDecimal(immediate)
-
-		formattedRegisterString := fmt.Sprintf("R%d, #%d", immNum, decimalNum)
-		return formattedString + fmt.Sprintf("%s\t", strconv.FormatInt(int64(ProgramCounter), 10)) +
-			"CBZ\t" + formattedRegisterString + "\n"
+		return BranchInstruction(instructionCode, "CBZ")
 	}
 
 	if strings.Index(opcode, "10110101") == 0 { //CBNZ instruction
-		opcode = instructionCode[:8]
-		immediate := instructionCode[8:27]
-		rd := instructionCode[27:]
-
-		immNum, _ := strconv.ParseInt(rd, 2, 32)
-
-		formattedString := fmt.Sprintf("%s\t", opcode+" "+immediate+" "+rd)
-
-		decimalNum := ConvertBinaryToDecimal(immediate)
-
-		formattedRegisterString := fmt.Sprintf("R%d, #%d", immNum, decimalNum)
-		return formattedString + fmt.Sprintf("%s\t", strconv.FormatInt(int64(ProgramCounter), 10)) +
-			"CBNZ\t" + formattedRegisterString + "\n"
+		return BranchInstruction(instructionCode, "CBNZ")
 	}
 
 	if strings.Index(opcode, "11001011000") == 0 { //SUB instruction NATHAN
@@ -177,6 +196,7 @@ func CreateString(instructionCode string) string {
 	}
 
 	if strings.Index(opcode, "00000000000") == 0 { //NOP instruction
+		instructionMap[int64(ProgramCounter)] = Instruction{programCounter: int64(ProgramCounter), instructionName: "NOP"}
 		return fmt.Sprintf("0000000000000000000000000000000\t%d\t%s\n", ProgramCounter, "NOP")
 	}
 
@@ -186,6 +206,7 @@ func CreateString(instructionCode string) string {
 
 	if strings.Index(opcode, "11111110110") == 0 { //BREAK instruction.
 		hasBreak = true
+		instructionMap[int64(ProgramCounter)] = Instruction{programCounter: int64(ProgramCounter), instructionName: "BREAK"}
 		return fmt.Sprintf("%s\t", "1 11111 10110 11110 11111 11111 100111") + fmt.Sprintf("%s\t", strconv.FormatInt(int64(ProgramCounter), 10)) +
 			"BREAK" + "\n"
 	}
@@ -205,6 +226,7 @@ func R_Instruction(instructionCode string, instructionName string) string { // C
 
 	formattedRegisterString := fmt.Sprintf("R%d, R%d, R%d", rd, rs, rt)
 
+	instructionMap[int64(ProgramCounter)] = Instruction{programCounter: int64(ProgramCounter), storeRegister: rd, targetRegister1: rs, targetRegister3: rt, instructionName: instructionName}
 	return formattedString + fmt.Sprintf("%s\t", strconv.FormatInt(int64(ProgramCounter), 10)) + instructionName + "\t" + formattedRegisterString + "\n"
 }
 
@@ -219,10 +241,12 @@ func R_Instruction_Modified(instructionCode string, instructionName string) stri
 		fmt.Sprintf("%06s", strconv.FormatInt(shamt, 2))+" "+fmt.Sprintf("%05s", strconv.FormatInt(rs, 2))+" "+fmt.Sprintf("%05s", strconv.FormatInt(rd, 2)))
 
 	formattedRegisterString := fmt.Sprintf("R%d, R%d, #%d", rd, rs, shamt)
+
+	instructionMap[int64(ProgramCounter)] = Instruction{programCounter: int64(ProgramCounter), storeRegister: rd, targetRegister1: rs, immediateValue: shamt, instructionName: instructionName}
 	return formattedString + fmt.Sprintf("%s\t", strconv.FormatInt(int64(ProgramCounter), 10)) + instructionName + "\t" + formattedRegisterString + "\n"
 }
 
-func D_Instruction(instructionCode string, instructionName string) string {
+func D_Instruction(instructionCode string, instructionName string) string { //Code for LDUR and STUR
 	opcode := instructionCode[0:11]
 	immediate := instructionCode[11:20]
 	op2 := instructionCode[20:22]
@@ -237,6 +261,7 @@ func D_Instruction(instructionCode string, instructionName string) string {
 
 	formattedRegisterString := fmt.Sprintf("R%d, [R%d, #%d]", rdNum, rtNum, immNum)
 
+	instructionMap[int64(ProgramCounter)] = Instruction{programCounter: int64(ProgramCounter), storeRegister: rdNum, targetRegister1: rtNum, immediateValue: immNum, instructionName: instructionName}
 	return formattedString + fmt.Sprintf("%s\t", strconv.FormatInt(int64(ProgramCounter), 10)) + instructionName + "\t" +
 		formattedRegisterString + "\n"
 }
@@ -255,6 +280,7 @@ func I_Instruction(instructionCode string, instructionName string) string { // C
 	formattedString := fmt.Sprintf("%s\t", opcode+" "+immediate+" "+rnNum+" "+rdNum)
 	formattedRegisterString := fmt.Sprintf("R%d, R%d", rd, rn)
 
+	instructionMap[int64(ProgramCounter)] = Instruction{programCounter: int64(ProgramCounter), storeRegister: rd, targetRegister1: rn, immediateValue: decimalNum, instructionName: instructionName}
 	return formattedString + fmt.Sprintf("%s\t", strconv.FormatInt(int64(ProgramCounter), 10)) + instructionName + "\t" +
 		formattedRegisterString + ", #" + strconv.FormatInt(decimalNum, 10) + "\n"
 }
@@ -289,7 +315,24 @@ func IM_Instruction(instructionCode string, instructionName string) string { //C
 		return ""
 	}
 
+	instructionMap[int64(ProgramCounter)] = Instruction{programCounter: int64(ProgramCounter), storeRegister: rdNum, targetRegister1: ImediateNum, immediateValue: shamtValue, instructionName: instructionName}
+
 	return fmt.Sprintf("%s%d\t%s\tR%s, %d, LSL %d\n", formattedString, ProgramCounter, instructionName, strconv.FormatInt(rdNum, 10), ImediateNum, shamtValue)
+}
+
+func BranchInstruction(instructionCode string, instructionName string) string { //Code for CBZ and CBNZ
+	opcode := instructionCode[:8]
+	immediate := instructionCode[8:27]
+	rd := instructionCode[27:]
+
+	immNum, _ := strconv.ParseInt(rd, 2, 32)
+	formattedString := fmt.Sprintf("%s\t", opcode+" "+immediate+" "+rd)
+	decimalNum := ConvertBinaryToDecimal(immediate)
+	formattedRegisterString := fmt.Sprintf("R%d, #%d", immNum, decimalNum)
+
+	instructionMap[int64(ProgramCounter)] = Instruction{programCounter: int64(ProgramCounter), targetRegister1: immNum, immediateValue: decimalNum, instructionName: instructionName}
+	return formattedString + fmt.Sprintf("%s\t", strconv.FormatInt(int64(ProgramCounter), 10)) +
+		instructionName + "\t" + formattedRegisterString + "\n"
 }
 
 // this takes a binary string, 2's complement or not (depending on the first bit) and converts it to decimal.
@@ -303,4 +346,24 @@ func ConvertBinaryToDecimal(binaryString string) int64 {
 		num -= mask
 		return num
 	}
+}
+
+func resizeRegisterList() {
+	//TODO... resize register list to fit whatever the maximum register accessed is.
+
+}
+
+func outputRegisters() string {
+	outputString := "registers:\n"
+
+	starterNum := 8
+	for ; starterNum <= maxRegisters; starterNum += 8 {
+		outputString += fmt.Sprintf("r%02d:", starterNum-8)
+		for i := starterNum - 8; i < starterNum; i++ {
+			outputString += fmt.Sprintf("\t%d", registers[i])
+		}
+		outputString += "\n"
+	}
+
+	return outputString
 }
